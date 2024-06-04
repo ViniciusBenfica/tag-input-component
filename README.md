0227ff3cb13146508501c3c1f34439cdcdec2c33
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2024-06-04 09:25:14 -0300

    ♻️ identification correction

--- README.md
+++ README.md
@@ -15,35 +15,32 @@ import React, { useState } from "react";
 import { TagInput } from "tag-input-component";
 
 export default function Home() {
-	const [tags, setTags] = useState<{id: string, text: string}[]>([])
+  const [tags, setTags] = useState<{id: string, text: string}[]>([])
 
-	function addItem(e: React.KeyboardEvent<HTMLInputElement>) {
-		if (e.key !== "Enter") return;
-		e.preventDefault();
-		const target = e.target as HTMLInputElement;
-		const newTag = { id: Date.now().toString(), text: target.value };
-		setTags((prev) => [...prev, newTag]);
-		target.value = "";
-	  }
+  function addItem(e: React.KeyboardEvent<HTMLInputElement>) {
+     if (e.key !== "Enter") return;
+     e.preventDefault();
+     const target = e.target as HTMLInputElement;
+     const newTag = { id: Date.now().toString(), text: target.value };
+     setTags((prev) => [...prev, newTag]);
+     target.value = "";
+  }
 	
-	  function removeItem(id: string) {
-		setTags((prev) => prev.filter((tag) => tag.id !== id));
-	  }
+  function removeItem(id: string) {
+     setTags((prev) => prev.filter((tag) => tag.id !== id));
+  }
 
-	return (
-		<main>
-			<div style={{width: '400px'}}>
-				<TagInput
-					tags={tags}
-					placeholder={'Tags'}
-					addTag={addItem}
-					removeTag={removeItem}
-				/>
-			</div>
-		</main>
-	);
+  return (
+    <main style={{width: '400px'}}>
+     <TagInput
+      tags={tags}
+      placeholder={'Tags'}
+      addTag={addItem}
+      removeTag={removeItem}
+     />
+    </main>
+  )
 }
-
 ```
 
 ## Next.js
--- src/index.tsx
+++ src/index.tsx
@@ -1,81 +1,89 @@
-import React, { InputHTMLAttributes, useEffect, useRef, useState } from 'react';
-import styles from './index.css';
-import CloseSvg from './svg/close';
+import React, { InputHTMLAttributes, useEffect, useRef, useState } from "react";
+import styles from "./index.css";
+import CloseSvg from "./svg/close";
 
 interface InputTagProps extends InputHTMLAttributes<HTMLInputElement> {
-  placeholder?: string;
-  tags: { id: string, text: string }[];
-  addTag?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  removeTag?: (item: string) => void;
-  maxTags?: number;
+	placeholder?: string;
+	tags: { id: string; text: string }[];
+	addTag?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+	removeTag?: (item: string) => void;
+	maxTags?: number;
 }
 
-const TagInput = ({ placeholder, tags, addTag, removeTag, maxTags, ...inputProps }: InputTagProps) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const spanRef = useRef<HTMLSpanElement>(null);
-  const [inputWidth, setInputWidth] = useState('20px');
+const TagInput = ({
+	placeholder,
+	tags,
+	addTag,
+	removeTag,
+	maxTags,
+	...inputProps
+}: InputTagProps) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+	const spanRef = useRef<HTMLSpanElement>(null);
+	const [inputWidth, setInputWidth] = useState("20px");
 
-  const updateInputWidth = () => {
-    if (spanRef.current && inputRef.current) {
-      const textContent = inputRef.current.value || tags.length === 0 && placeholder || '';
-      spanRef.current.textContent = textContent;
-      const spanWidth = spanRef.current.offsetWidth;
-      setInputWidth(`${spanWidth + 20}px`);
-    }
-  };
+	const updateInputWidth = () => {
+		if (spanRef.current && inputRef.current) {
+			const textContent =
+				inputRef.current.value || (tags.length === 0 && placeholder) || "";
+			spanRef.current.textContent = textContent;
+			const spanWidth = spanRef.current.offsetWidth;
+			setInputWidth(`${spanWidth + 20}px`);
+		}
+	};
 
-  useEffect(() => {
-    updateInputWidth();
-    const handleInput = () => updateInputWidth();
-    if (inputRef.current) {
-      inputRef.current.addEventListener('input', handleInput);
-    }
-    return () => {
-      if (inputRef.current) {
-        inputRef.current.removeEventListener('input', handleInput);
-      }
-    };
-  }, [placeholder, tags]);
+	useEffect(() => {
+		updateInputWidth();
+		const handleInput = () => updateInputWidth();
+		if (inputRef.current) {
+			inputRef.current.addEventListener("input", handleInput);
+		}
+		return () => {
+			if (inputRef.current) {
+				inputRef.current.removeEventListener("input", handleInput);
+			}
+		};
+	}, [placeholder, tags]);
 
-  useEffect(() => {
-    updateInputWidth();
-  }, [tags]);
+	useEffect(() => {
+		updateInputWidth();
+	}, [tags]);
 
-  const handleContainerClick = () => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-      inputRef.current.select();
-    }
-  };
+	const handleContainerClick = () => {
+		if (inputRef.current) {
+			inputRef.current.focus();
+			inputRef.current.select();
+		}
+	};
 
-  return (
-    <>
-      <div className={styles.inputContainer} onClick={handleContainerClick}>
-        {tags?.map((item, index) => (
-          <div key={index} className={styles.tag}>
-            <div>{item?.text}</div>
-            {removeTag && (
-              <button onClick={() => removeTag(item?.id)}>
-                <CloseSvg />
-              </button>
-            )}
-          </div>
-        ))}
-        {maxTags === undefined || tags.length < maxTags ? (
-        <div className={styles.inputWrapper}>
-          <input
-            ref={inputRef}
-            style={{ width: inputWidth }}
-            placeholder={tags.length === 0 ? placeholder : ''}
-            onKeyDown={addTag}
-            {...inputProps}
-          />
-          <span ref={spanRef} className={styles.hiddenSpan}></span>
-        </div>
-      ) : null}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={styles.inputContainer} onClick={handleContainerClick}>
+				{tags?.map((item, index) => (
+					<div key={index} className={styles.tag}>
+						<div>{item?.text}</div>
+						{removeTag && (
+							<button onClick={() => removeTag(item?.id)}>
+								<CloseSvg />
+							</button>
+						)}
+					</div>
+				))}
+				{maxTags === undefined || tags.length < maxTags ? (
+					<div className={styles.inputWrapper}>
+						<input
+							ref={inputRef}
+							style={{ width: inputWidth }}
+							placeholder={tags.length === 0 ? placeholder : ""}
+							onKeyDown={addTag}
+							{...inputProps}
+						/>
+						<span ref={spanRef} className={styles.hiddenSpan}></span>
+					</div>
+				) : null}
+			</div>
+		</>
+	);
 };
 
 export default TagInput;
